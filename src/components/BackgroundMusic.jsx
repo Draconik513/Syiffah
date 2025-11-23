f243da3475d52ee7b6e4f32eee1740edccf8dff3
DraconikCatastrophe <abenk223jr@gmail.com>
2025-11-23 21:06:51 +0800

    Update BackgroundMusic.jsx

--- src/components/BackgroundMusic.jsx
+++ src/components/BackgroundMusic.jsx
@@ -44,11 +44,11 @@ const BackgroundMusic = () => {
           overflow: "hidden",
         }}
       >
-        <iframe
+       <iframe
           ref={iframeRef}
           width="1"
           height="1"
-          src="https://www.youtube.com/embed/DjMu_4O_EYk?autoplay=1&loop=1&playlist=DjMu_4O_EYk&enablejsapi=1&mute=0"
+          src="https://www.youtube.com/embed/pegAjUafgME?autoplay=1&loop=1&playlist=pegAjUafgME&enablejsapi=1&mute=0"
           title="Background Music"
           frameBorder="0"
           allow="autoplay; encrypted-media"
